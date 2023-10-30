43bbd807f04b0fc8ed66ed0950cf162e9aab9cf7
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-10-30 19:35:25 -0300

    feat: remove logs to be send to datadog

--- src/settings/logger.settings.ts
+++ src/settings/logger.settings.ts
@@ -1,16 +1,9 @@
 import winston from 'winston';
-import { hostname } from 'os';
 import TransportElastic from 'elasticsearch-transport'
 
-const { DD_API_KEY, APP_ENV, ELASTIC_PASSWORD } = process.env
+const { APP_ENV, ELASTIC_PASSWORD } = process.env
 
 const serviceName = 'authentication'
-
-const httpOptions: winston.transports.HttpTransportOptions = {
-    host: 'http-intake.logs.datadoghq.com',
-    path: `/api/v2/logs?dd-api-key=${DD_API_KEY}&ddsource=nodejs&service=${serviceName}-${APP_ENV}&hostname=${hostname()}`,
-    ssl: true,
-}
 
 const chooseTransport = () => {
     const transports: winston.transport[] = [
@@ -26,11 +19,7 @@ const chooseTransport = () => {
         }),
     ]
 
-    if (APP_ENV === 'production') {
-        transports.push(
-            new winston.transports.Http(httpOptions)
-        )
-    } else {
+    if (APP_ENV !== 'production') {
         transports.push(
             new winston.transports.Console(),
         )
